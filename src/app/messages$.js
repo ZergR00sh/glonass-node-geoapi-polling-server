14388b562a7fb2542b97abbe796d0c975e1bdb26
Mike Faraponov <java.math.random@bk.ru>
2016-12-19 12:05:41 +0200

    Refactored

--- src/app/messages$.js
+++ src/app/messages$.js
@@ -5,31 +5,28 @@ const redisLogger = require('../loggers/redis-logger');
 const redisFactory = require('../db/redis-factory');
 
 const messages$ = Rx.Observable.create(function(observer) {
-  const client = redisFactory();
+  const subscriber = redisFactory();
 
-  client.on('message', (channel, message) => {
+  subscriber.on('message', (channel, message) => {
     redisLogger.info('received message on ' + channel + ': ' + message);
-
-    if (channel === CHANNEL.HAS_USERS) {
-      switch(message) {
-        case CHANNEL.MSGS.FIRST_USER_ARRIVED:
-          observer.next(true);
-          break;
-        case CHANNEL.MSGS.LAST_USER_LEFT:
-          observer.next(false);
-          break;
-        default:
-          appLogger.debug('UNHENDLED_MESSAGE');
-          observer.error(new Error('UNHENDLED_MESSAGE'));
-      }
+    switch(message) {
+      case CHANNEL.MSGS.FIRST_USER_ARRIVED:
+        observer.next(true);
+        break;
+      case CHANNEL.MSGS.LAST_USER_LEFT:
+        observer.next(false);
+        break;
+      default:
+        appLogger.debug('UNHENDLED_MESSAGE');
+        observer.error(new Error('UNHENDLED_MESSAGE'));
     }
   });
 
-  client.subscribe(CHANNEL.HAS_USERS);
+  subscriber.subscribe(CHANNEL.HAS_USERS);
 
   return () => {
-    client.unsubscribe();
-    client.quit();
+    subscriber.unsubscribe();
+    subscriber.quit();
   };
 });
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const DevicesPublisher = require('./app');
 const CHANNEL = require('./constants').CHANNEL;
+
 const publisher = new DevicesPublisher(CHANNEL.GEO_STATE);
 
 publisher.start();
